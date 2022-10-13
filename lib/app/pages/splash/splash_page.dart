72aa2a8d4f2981c2182ad893a92530fc128f5aea
WilliamJSS <williamsena088@gmail.com>
2022-10-12 22:56:13 -0300

    feat: criados helpers

--- lib/app/pages/splash/splash_page.dart
+++ lib/app/pages/splash/splash_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fwc_album_app/app/core/ui/components/button.dart';
 import 'package:fwc_album_app/app/core/ui/components/rounded_button.dart';
+import 'package:fwc_album_app/app/core/ui/helpers/loader.dart';
+import 'package:fwc_album_app/app/core/ui/helpers/messages.dart';
 import 'package:fwc_album_app/app/core/ui/styles/button_styles.dart';
 import 'package:fwc_album_app/app/core/ui/styles/text_styles.dart';
 
@@ -11,7 +13,8 @@ class SplashPage extends StatefulWidget {
   State<SplashPage> createState() => _SplashPageState();
 }
 
-class _SplashPageState extends State<SplashPage> {
+class _SplashPageState extends State<SplashPage>
+    with Loader<SplashPage>, Messages<SplashPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -24,17 +27,23 @@ class _SplashPageState extends State<SplashPage> {
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                showError('Algum erro');
+              },
               style: context.buttonStyles.yellowButton,
               child: const Text('Salvar'),
             ),
             ElevatedButton(
-              onPressed: () {},
+              onPressed: () {
+                showInfo('Preste atenção aqui');
+              },
               style: context.buttonStyles.primaryButton,
               child: const Text('Salvar'),
             ),
             OutlinedButton(
-              onPressed: () {},
+              onPressed: () {
+                showSuccess('Tudo certo!');
+              },
               style: context.buttonStyles.yellowOutlinedButton,
               child: const Text('Salvar'),
             ),
@@ -57,7 +66,12 @@ class _SplashPageState extends State<SplashPage> {
             ),
             RoundedButton(
               icon: Icons.add,
-              onPressed: () {},
+              onPressed: () {
+                showLoader();
+                Future.delayed(
+                  const Duration(seconds: 2),
+                ).then((_) => hideLoader());
+              },
             ),
           ],
         ),
